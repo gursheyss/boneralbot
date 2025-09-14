375a960025ba9f0aaab3439a05c52192be96d206
Gurshan <rkwarya@gmail.com>
2025-09-13 20:16:41 -0700

    parallel gen

--- lib/image.ts
+++ lib/image.ts
@@ -9,36 +9,38 @@ export function generateImage(
 ): ResultAsync<{ seedream: Buffer; nanoBanana: Buffer }, Error> {
   return ResultAsync.fromPromise(
     (async () => {
-      // Generate with seedream-4
       const seedreamInput = {
         prompt: prompt,
         image_input: imageUrls
       } as const
 
-      const seedreamOutput = await replicate.run('bytedance/seedream-4', {
-        input: seedreamInput
-      })
-
-      // Generate with nano-banana
       const nanoBananaInput = {
         prompt: prompt,
         image_input: imageUrls,
         output_format: 'jpg'
       } as const
 
-      const nanoBananaOutput = await replicate.run('google/nano-banana', {
-        input: nanoBananaInput
-      })
+      const [seedreamOutput, nanoBananaOutput] = await Promise.all([
+        replicate.run('bytedance/seedream-4', {
+          input: seedreamInput
+        }),
+        replicate.run('google/nano-banana', {
+          input: nanoBananaInput
+        })
+      ])
 
-      // @ts-expect-error - yes it does
-      const seedreamResponse = await fetch(seedreamOutput[0].url())
-      const seedreamBuffer = Buffer.from(await seedreamResponse.arrayBuffer())
+      // Fetch both images in parallel
+      const [seedreamResponse, nanoBananaResponse] = await Promise.all([
+        // @ts-expect-error - yes it does
+        fetch(seedreamOutput[0].url()),
+        // @ts-expect-error - yes it does
+        fetch(nanoBananaOutput.url())
+      ])
 
-      // @ts-expect-error - yes it does
-      const nanoBananaResponse = await fetch(nanoBananaOutput.url())
-      const nanoBananaBuffer = Buffer.from(
-        await nanoBananaResponse.arrayBuffer()
-      )
+      const [seedreamBuffer, nanoBananaBuffer] = await Promise.all([
+        seedreamResponse.arrayBuffer().then((ab) => Buffer.from(ab)),
+        nanoBananaResponse.arrayBuffer().then((ab) => Buffer.from(ab))
+      ])
 
       return {
         seedream: seedreamBuffer,
